d57af1aea573f9dbe7fa68d9bb26cd1f45e63a93
arshad47878 <qureshiarshad47878@gmail.com>
2022-08-23 17:38:51 +0530

    address all comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :find_articles, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def new
@@ -12,7 +13,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -26,8 +26,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-
     if @article.update(article_params)
       redirect_to @article
     else
@@ -36,14 +34,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
 
     redirect_to articles_path
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :text)
-    end
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+  def find_articles
+    @article = Article.find(params[:id])
+  end
 end
