2000c593b93578c492527fd74465eeb7bcecd794
arshad47878 <qureshiarshad47878@gmail.com>
2022-08-23 16:19:17 +0530

    changes made again

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,42 +4,46 @@ class ArticlesController < ApplicationController
   end
 
   def show
-	@article = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def new
-	@article = Article.new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
-	@article = Article.new(articles_params)
+    @article = Article.new(article_params)
 
-	if @article.save
-	  redirect_to @article
-	else
-	  render 'New'
-	end
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def update
     @article = Article.find(params[:id])
 
-	if @article.update(articles_params)
-	  redirect_to @article
-	else
-	  render 'edit'
-	end
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def destroy
     @article = Article.find(params[:id])
-	@article.destroy
+    @article.destroy
 
-	redirect_to articles_path
+    redirect_to articles_path
   end
 
   private
-  def articles_params
-	params.require(:article).permit(:title, :text)
-  end
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
