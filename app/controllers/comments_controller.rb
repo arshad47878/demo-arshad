84e26d5bc981bccf0ad283ce4b04e0866d5fd657
arshad47878 <qureshiarshad47878@gmail.com>
2022-08-30 16:05:28 +0530

    comment changes 2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,11 @@
 class CommentsController < ApplicationController
+  before_action :find_articles, only: [:create, :destroy]
   def create
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to articles_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to articles_path(@article)
@@ -15,5 +14,8 @@ class CommentsController < ApplicationController
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body)
+  end
+  def find_articles
+    @article = Article.find(params[:id])
   end
 end
