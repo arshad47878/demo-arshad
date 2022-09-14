a53e2907aa0a198c5ccf7ec02f846d905503c6d9
arshad47878 <qureshiarshad47878@gmail.com>
2022-09-14 14:14:54 +0530

    sending mail using action mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  has_many :comments, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :omniauthable, omniauth_providers: [:google_oauth2]
+
+
 
   def full_name
     "#{first_name} #{last_name}"
